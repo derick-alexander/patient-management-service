3e03ff07e6c47b8cd19af50b1fe41161bd1643a1
derick-alexander <derickawsalexander@gmail.com>
2024-12-18 00:59:55 +0530

    first commit

---
+++ src/main/java/com/cmm707/meditrack/patient_management_service/PatientManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.patient_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cmm707/meditrack/patient_management_service/PatientManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmm707.meditrack.patient_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
